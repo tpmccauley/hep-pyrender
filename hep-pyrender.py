56813158bb57d8cce80dba113cb2b0e3de05261d
Tom McCauley <tpmccauley@gmail.com>
2021-05-20 16:10:11 +0100

    lights

--- hep-pyrender.py
+++ hep-pyrender.py
@@ -6,6 +6,10 @@ import numpy as np
 def hep_pyrender():
 
     trimesh.util.attach_to_log()
+    
+    eb_model = trimesh.load(
+        'assets/detector/EB.obj'
+    ) 
 
     eep_model = trimesh.load(
         'assets/detector/EEplus.obj'
@@ -15,10 +19,6 @@ def hep_pyrender():
         'assets/detector/EEminus.obj'
     )
     
-    eb_model = trimesh.load(
-        'assets/detector/EB.obj'
-    ) 
-
     beampipe_model = trimesh.load(
         'assets/detector/beampipe.obj'
     )
@@ -26,25 +26,45 @@ def hep_pyrender():
     EB = pyrender.Mesh.from_trimesh(eb_model)
     EEp = pyrender.Mesh.from_trimesh(eep_model)
     EEm = pyrender.Mesh.from_trimesh(eem_model)
-
     BP = pyrender.Mesh.from_trimesh(beampipe_model)
-    
-    scene = pyrender.Scene()
 
-    light = pyrender.PointLight(
+    scene = pyrender.Scene(
+        ambient_light=[0.03, 0.03, 0.03],
+        bg_color=[0.0, 0.0, 0.0],
+    ) 
+    
+    viewport_size=[1600,900]
+    
+    dl = pyrender.light.DirectionalLight(
         color=[1.0, 1.0, 1.0],
-        intensity=10.0
+        intensity=5.0,
+        name='directional light',
     )
+    
+    light_node1 = pyrender.Node(
+        light=dl,
+        translation=[1,1,1],
+        scale=[10,10,10],
+    )
+
+    light_node2 = pyrender.Node(
+        light=dl,
+        translation=[-1,-1,-1],
+        scale=[10,10,10],
+    )
+    
+    scene.add_node(light_node1)
+    scene.add_node(light_node2)
     
     scene.add(EB)
     scene.add(EEp)
     scene.add(EEm)
     scene.add(BP)
-    
-    scene.add(light)
-    
-    pyrender.Viewer(scene)
-    
 
+    pyrender.Viewer(
+        scene,
+        viewport_size=viewport_size,
+    )
+    
 if __name__ == '__main__':
     hep_pyrender()
